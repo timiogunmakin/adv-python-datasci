31cdd6dc6046bd722089d1e720318f1ff22a39e7
Timi <timioluwatimi@gmail.com>
2022-01-18 11:33:43 -0500

    Update for section 2 on modularity

--- notebooks/my_module.py
+++ notebooks/my_module.py
@@ -8,11 +8,7 @@ def get_features_and_target(csv_file, target_col):
     
     adult_census = pd.read_csv(csv_file)
     
-    raw_features = adult_census.drop(columns=target_col)
-    numeric_features = raw_features.select_dtypes(np.number)
-    feature_cols = numeric_features.columns.values
-
-    features = adult_census[feature_cols]
+    features = adult_census.drop(columns=target_col)
     target = adult_census[target_col]
     
     return (features, target)
